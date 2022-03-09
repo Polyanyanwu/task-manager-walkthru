7dbe3db69611085637d76b0e46eab3f148b04fce
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-03-09 12:02:42 +0000

    Add data models

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,6 +1,8 @@
 """routes """
+
 from flask import render_template
-from taskmanager import app, db # noqa
+from taskmanager import app, db
+from taskmanager.models import Category, Task
 
 
 @app.route("/")

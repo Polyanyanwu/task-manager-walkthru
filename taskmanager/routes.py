20dbc9547e9b80d5e07660582d840000ab89f6c9
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-03-09 16:46:49 +0000

    Edit category

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -12,14 +12,28 @@ def home():
 
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    """ categories """
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories)
 
 
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
+    """ create new category """
     if request.method == "POST":
         category = Category(category_name=request.form.get("category_name"))
         db.session.add(category)
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    """edit category """
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
